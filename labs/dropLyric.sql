aa528cc2bd0a9ff54979431ac70efdf0105563c9
Jeff Lehman <jlehman@huntington.edu>
2025-11-18 11:36:39 -0500

    Create dropLyric.sql

---
+++ labs/dropLyric.sql
@@ -0,0 +1,9 @@
+# deletes Lyric database
+drop table Artists;
+drop table Genre;
+drop table Members;
+drop table SalesPeople;
+drop table Studios;
+drop table Titles;
+drop table Tracks;
+drop table XrefArtistsMembers;
